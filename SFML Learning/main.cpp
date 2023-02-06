d789cf8e4200f5e399ad367773db21d48ceb856b
HonouredOne <88534930+HonouredOne@users.noreply.github.com>
2023-02-05 21:16:48 -0700

    testing linux

--- SFML Learning/main.cpp
+++ SFML Learning/main.cpp
@@ -34,6 +34,10 @@ int main()
   return 0;
 }
 
-// Use this code first: g++ -IC:\SFML\include -c main.cpp -o main.o
-// Then use this to compile the exe: g++ -LC:\SFML\lib -o main.exe main.o -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
-// Then run main.exe
+// On Windows:  Use this code first: g++ -IC:\SFML\include -c main.cpp -o main.o
+//              Then use this to compile the exe: g++ -LC:\SFML\lib -o main.exe main.o -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+//              Then run main.exe
+// 
+// On Linux:    Use this code first: g++ -c main.cpp
+//              Then use this to compile the app: g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+//              Then run: ./sfml-app
