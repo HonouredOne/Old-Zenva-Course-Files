43bd56fe69b0e90020868b3d16317c36c1a57037
HonouredOne <jameswrightlpa@gmail.com>
2023-02-15 19:23:13 -0700

    built game world

---
+++ SFML Learning/gameWorld.cpp
@@ -0,0 +1,41 @@
+#include "gameWorld.h"
+
+GameWorld::GameWorld() {
+  damage = 10;
+}
+
+bool GameWorld::loadBackground() {
+  if (!backgroundTexture.loadFromFile("assets/background.png")) {
+    std::cout << "Could not load background image" << std::endl;
+    return false;
+  }
+  background.setTexture(backgroundTexture);
+  
+  return true;
+}
+
+bool GameWorld::performSetup() {
+  isGameOver = false;
+  return loadBackground();
+}
+
+bool GameWorld::runGame() {
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "Point and Click Game");
+  
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+        return false;
+      } 
+    }
+    
+    window.clear();
+    window.draw(background);
+    window.display();
+    }
+  return false;
+}
