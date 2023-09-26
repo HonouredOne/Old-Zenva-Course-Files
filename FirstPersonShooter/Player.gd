ab9bacd91062b8b56c7c0fc1299a690160b67fe1
HonouredOne <jameswrightlpa@gmail.com>
2023-09-26 14:52:39 -0500

    added movement function and physics

--- FirstPersonShooter/Player.gd
+++ FirstPersonShooter/Player.gd
@@ -25,3 +25,41 @@ onready var camera : Camera = get_node("Camera")
 onready var muzzle : Spatial = get_node("Camera/Muzzle")
 
 func _physics_process(delta):
+	
+	# reset x and z velocity
+	vel.x = 0
+	vel.z = 0
+	
+	var input = Vector2()
+	
+	# movement inputs
+	if Input.is_action_pressed("move_forward"):
+		input.y -= 1
+	if Input.is_action_pressed("move_backward"):
+		input.y += 1
+	if Input.is_action_pressed("move_left"):
+		input.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input.x += 1
+		
+	input = input.normalized()
+	
+	# get the forward and right directions
+	var forward = global_transform.basis.z
+	var right = global_transform.basis.x
+	
+	var relativeDir = (forward * input.y + right * input.x)
+	
+	# set the velocity
+	vel.x = relativeDir.x * moveSpeed
+	vel.z = relativeDir.z * moveSpeed
+	
+	# apply gravity
+	vel.y -= gravity * delta
+	
+	# move the player
+	vel = move_and_slide(vel, Vector3.UP)
+	
+	# jumping
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y = jumpForce
